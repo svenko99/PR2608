d94e5f4e6d2d31c95572160fe161f394be0d5eed
svenko99 <107575361+svenko99@users.noreply.github.com>
2026-03-17 19:19:41 +0100

    Add pull rebase to shell script

--- src/scripts/run_and_commit.sh
+++ src/scripts/run_and_commit.sh
@@ -21,6 +21,7 @@ echo "$OUTPUT" >> "$LOG_FILE"
 COMMIT_MSG=$(echo "$OUTPUT" | tail -n 3)
 
 cd "$REPO_DIR"
+git pull --rebase
 git add data/data.csv data/changes.csv
 git commit -m "$COMMIT_MSG"
 git push
