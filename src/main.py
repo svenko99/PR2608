adedf25a518ea8d7a228921c2f2ab60c8ce72bfe
svenko99 <107575361+svenko99@users.noreply.github.com>
2026-03-18 22:11:22 +0100

    Reverse the order of print message

--- src/main.py
+++ src/main.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     listings = scraper.extract_multiple_pages()  # Prebere vse strani (dodaj parameter max_page če hočeš omejiti do katere strani gre)
     changes, new_count = scraper.update_csv_database(listings)
 
-    print(f"Posodobljen data.csv z {len(listings)} trenutno videnimi oglasi.")
     print(f"Novih oglasov: {new_count}.")
+    print(f"Posodobljen data.csv z {len(listings)} trenutno videnimi oglasi.")
     print(f"Posodobljen changes.csv z {len(changes)} zaznanimi spremembami.")
